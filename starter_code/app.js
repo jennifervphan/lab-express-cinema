7aef6f5c36827ab8deaee60276f7c1d34d3f57d6
Jennifer V Phan <jennyphan181295@gmail.com>
2019-07-17 15:16:49 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,28 +1,37 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const app = express();
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const Schema = mongoose.Schema;
 
+const Movie = mongoose.model('movies', new Schema({
+    title: String,
+    director: String,
+    description: String,
+    showtimes: Array,
+    stars: Array,
+    image: String,
+}), "movies");
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+    .connect('mongodb://localhost/yourdatabase', { useNewUrlParser: true })
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => {
+        console.error('Error connecting to mongo', err)
+    });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-const app = express();
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -33,11 +42,11 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -54,5 +63,27 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+app.get('/movies', function(req, res) {
+    Movie.find()
+        .then(movies => {
+            console.log(movies);
+            res.render('movies', { movies });
+        })
+})
+
+app.get('/movies/:id', function(req, res) {
+    let idNum = req.params.id;
+    Movie.find({ _id: idNum })
+        .then((movieChosen) => {
+            console.log(movieChosen[0].title);
+            let asdf = movieChosen[0]
+            console.log(Object.keys(movieChosen[0]));
+            res.render('movieDetail', { movieChosen });
+        })
+})
+
+app.listen(3000, () => {
+    console.log("App listening")
+})
 
 module.exports = app;
