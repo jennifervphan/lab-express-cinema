9a0a7a67f3d32af04dd78595f0a5f658fc491a78
Jennifer V Phan <jennyphan181295@gmail.com>
2019-07-18 10:34:00 +0200

    corrected

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,8 +9,8 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
-const Schema = mongoose.Schema;
 
+const Schema = mongoose.Schema;
 const Movie = mongoose.model('movies', new Schema({
     title: String,
     director: String,
@@ -32,7 +32,6 @@ mongoose
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -40,25 +39,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public'),
     sourceMap: true
 }));
 
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
-
-
 
 const index = require('./routes/index');
 app.use('/', index);
@@ -75,9 +68,6 @@ app.get('/movies/:id', function(req, res) {
     let idNum = req.params.id;
     Movie.find({ _id: idNum })
         .then((movieChosen) => {
-            console.log(movieChosen[0].title);
-            let asdf = movieChosen[0]
-            console.log(Object.keys(movieChosen[0]));
             res.render('movieDetail', { movieChosen });
         })
 })
